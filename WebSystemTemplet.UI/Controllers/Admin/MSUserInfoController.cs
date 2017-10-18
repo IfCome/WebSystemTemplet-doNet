7b763ce8978fbf10f41d2f5ba172def8fcd1f4d2
yang_ <yang_@DESKTOP-0GES4F9>
2017-10-18 12:19:24 +0800

    完善页面，添加菜单

---
+++ WebSystemTemplet.UI/Controllers/Admin/MSUserInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSystemTemplet.UI.Controllers.Admin
+{
+    public class MSUserInfoController : PCBaseController
+    {
+        // GET: MSUserInfo
+        public ActionResult TeacherList()
+        {
+            return View("~/Views/Admin/MSUserInfo/TeacherList.cshtml");
+        }
+    }
+}
--- WebSystemTemplet.UI/Resources/adminassets/js/app.js
+++ WebSystemTemplet.UI/Resources/adminassets/js/app.js
@@ -1,31 +1,67 @@
-$(function() {
-    // 读取body data-type 判断是哪个页面然后执行相应页面方法，方法在下面。
-    var dataType = $('body').attr('data-type');
-    console.log(dataType);
-    for (key in pageData) {
-        if (key == dataType) {
-            pageData[key]();
-        }
-    }
-    //     // 判断用户是否已有自己选择的模板风格
-    //    if(storageLoad('SelcetColor')){
-    //      $('body').attr('class',storageLoad('SelcetColor').Color)
-    //    }else{
-    //        storageSave(saveSelectColor);
-    //        $('body').attr('class','theme-black')
-    //    }
+$(function () {
 
+    // 侧边菜单开关
     autoLeftNav();
-    $(window).resize(function() {
+    $(window).resize(function () {
         autoLeftNav();
-        console.log($(window).width())
+        //console.log($(window).width())
     });
 
-    //    if(storageLoad('SelcetColor')){
+    // 全屏
+    var $fullText = $('.admin-fullText');
+    $('#admin-fullscreen').on('click', function () {
+        $.AMUI.fullscreen.toggle();
+    });
+    $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function () {
+        $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
+    });
 
-    //     }else{
-    //       storageSave(saveSelectColor);
-    //     }
+    // 风格切换
+    $('.tpl-skiner-toggle').on('click', function () {
+        $('.tpl-skiner').toggleClass('active');
+    })
+    $('.tpl-skiner-content-bar').find('span').on('click', function () {
+        $('body').attr('class', $(this).attr('data-color'))
+        saveSelectColor.Color = $(this).attr('data-color');
+        // 保存选择项
+        storageSave(saveSelectColor);
+
+    })
+
+    // 侧边菜单
+    $('.sidebar-nav-sub-title').on('click', function () {
+        $(this).siblings('.sidebar-nav-sub').slideToggle(80)
+            .end()
+            .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
+    })
+    $('.sidebar-nav-link>a').on('click', function () {
+        saveSelectMenu.MenuId = $(this).prop('id');
+        storageSave(saveSelectMenu);
+    })
+
+    // 判断用户是否已有自己选择的模板风格
+    if (storageLoad('SelcetColor')) {
+        $('body').attr('class', storageLoad('SelcetColor').Color)
+    } else {
+        storageSave(saveSelectColor);
+        $('body').attr('class', 'theme-white')
+    }
+
+    // 设置当前选中的菜单
+    var selectMenuId;
+    if (storageLoad('SelcetMenu')) {
+        selectMenuId = storageLoad('SelcetMenu').MenuId;
+    } else {
+        storageSave(saveSelectMenu);
+        selectMenuId = 'menu-home';
+    }
+    var selectMenu = $('#' + selectMenuId);
+    if (selectMenu.closest('ul').hasClass('sidebar-nav-sub')) {
+        selectMenu.addClass('sub-active').closest('ul').css('display','block').prev().addClass('active');
+    } else {
+        selectMenu.addClass('active')
+    }
+
 })
 
 
@@ -116,15 +152,15 @@ var pageData = {
                 data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             }],
             yAxis: [{
-                    type: 'value',
-                    name: '水量',
-                    min: 0,
-                    max: 250,
-                    interval: 50,
-                    axisLabel: {
-                        formatter: '{value} ml'
-                    }
-                },
+                type: 'value',
+                name: '水量',
+                min: 0,
+                max: 250,
+                interval: 50,
+                axisLabel: {
+                    formatter: '{value} ml'
+                }
+            },
                 {
                     type: 'value',
                     name: '温度',
@@ -137,10 +173,10 @@ var pageData = {
                 }
             ],
             series: [{
-                    name: '蒸发量',
-                    type: 'bar',
-                    data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-                },
+                name: '蒸发量',
+                type: 'bar',
+                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            },
                 {
                     name: '降水量',
                     type: 'bar',
@@ -167,15 +203,15 @@ var pageData = {
                 data: ['某软件', '某主食手机', '某水果手机', '降水量', '蒸发量']
             },
             radar: [{
-                    indicator: [
-                        { text: '品牌', max: 100 },
-                        { text: '内容', max: 100 },
-                        { text: '可用性', max: 100 },
-                        { text: '功能', max: 100 }
-                    ],
-                    center: ['25%', '40%'],
-                    radius: 80
-                },
+                indicator: [
+                    { text: '品牌', max: 100 },
+                    { text: '内容', max: 100 },
+                    { text: '可用性', max: 100 },
+                    { text: '功能', max: 100 }
+                ],
+                center: ['25%', '40%'],
+                radius: 80
+            },
                 {
                     indicator: [
                         { text: '外观', max: 100 },
@@ -188,7 +224,7 @@ var pageData = {
                     center: ['50%', '60%'],
                 },
                 {
-                    indicator: (function() {
+                    indicator: (function () {
                         var res = [];
                         for (var i = 1; i <= 12; i++) {
                             res.push({ text: i + '月', max: 100 });
@@ -200,23 +236,23 @@ var pageData = {
                 }
             ],
             series: [{
-                    type: 'radar',
-                    tooltip: {
-                        trigger: 'item'
-                    },
-                    itemStyle: { normal: { areaStyle: { type: 'default' } } },
-                    data: [{
-                        value: [60, 73, 85, 40],
-                        name: '某软件'
-                    }]
+                type: 'radar',
+                tooltip: {
+                    trigger: 'item'
                 },
+                itemStyle: { normal: { areaStyle: { type: 'default' } } },
+                data: [{
+                    value: [60, 73, 85, 40],
+                    name: '某软件'
+                }]
+            },
                 {
                     type: 'radar',
                     radarIndex: 1,
                     data: [{
-                            value: [85, 90, 90, 95, 95],
-                            name: '某主食手机'
-                        },
+                        value: [85, 90, 90, 95, 95],
+                        name: '某主食手机'
+                    },
                         {
                             value: [95, 80, 95, 90, 93],
                             name: '某水果手机'
@@ -228,9 +264,9 @@ var pageData = {
                     radarIndex: 2,
                     itemStyle: { normal: { areaStyle: { type: 'default' } } },
                     data: [{
-                            name: '降水量',
-                            value: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3],
-                        },
+                        name: '降水量',
+                        value: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3],
+                    },
                         {
                             name: '蒸发量',
                             value: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 35.6, 62.2, 32.6, 20.0, 6.4, 3.3]
@@ -265,20 +301,20 @@ var pageData = {
                 type: 'value'
             }],
             series: [{
-                    name: '邮件',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: { normal: {} },
-                    data: [120, 132, 101, 134, 90, 230, 210],
-                    itemStyle: {
-                        normal: {
-                            color: '#59aea2'
-                        },
-                        emphasis: {
+                name: '邮件',
+                type: 'line',
+                stack: '总量',
+                areaStyle: { normal: {} },
+                data: [120, 132, 101, 134, 90, 230, 210],
+                itemStyle: {
+                    normal: {
+                        color: '#59aea2'
+                    },
+                    emphasis: {
 
-                        }
                     }
-                },
+                }
+            },
                 {
                     name: '媒体',
                     type: 'line',
@@ -310,38 +346,15 @@ var pageData = {
 }
 
 
-// 风格切换
-
-$('.tpl-skiner-toggle').on('click', function() {
-    $('.tpl-skiner').toggleClass('active');
-})
-
-$('.tpl-skiner-content-bar').find('span').on('click', function() {
-    $('body').attr('class', $(this).attr('data-color'))
-    saveSelectColor.Color = $(this).attr('data-color');
-    // 保存选择项
-    storageSave(saveSelectColor);
-
-})
-
-
-
-
 // 侧边菜单开关
-
-
 function autoLeftNav() {
-
-
-
-    $('.tpl-header-switch-button').on('click', function() {
+    $('.tpl-header-switch-button').on('click', function () {
         if ($('.left-sidebar').is('.active')) {
             if ($(window).width() > 1024) {
                 $('.tpl-content-wrapper').removeClass('active');
             }
             $('.left-sidebar').removeClass('active');
         } else {
-
             $('.left-sidebar').addClass('active');
             if ($(window).width() > 1024) {
                 $('.tpl-content-wrapper').addClass('active');
@@ -357,9 +370,27 @@ function autoLeftNav() {
 }
 
 
-// 侧边菜单
-$('.sidebar-nav-sub-title').on('click', function() {
-    $(this).siblings('.sidebar-nav-sub').slideToggle(80)
-        .end()
-        .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
-})
+var saveSelectColor = {
+    'Name': 'SelcetColor',
+    'Color': 'theme-white'
+}
+var saveSelectMenu = {
+    'Name': 'SelcetMenu',
+    'MenuId': 'menu-home'
+}
+
+// 本地缓存
+function storageSave(objectData) {
+    localStorage.setItem(objectData.Name, JSON.stringify(objectData));
+}
+
+function storageLoad(objectName) {
+    if (localStorage.getItem(objectName)) {
+        return JSON.parse(localStorage.getItem(objectName))
+    } else {
+        return false
+    }
+}
+
+
+
